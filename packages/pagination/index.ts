a5a100e8f83f7a7cc7d18dc625754eb56fa94021
ssw1992 <ssw1992@qq.com>
2024-10-17 07:10:08 +0000

    feat: 分页hook，针对连续列表分页

--- packages/pagination/index.ts
+++ packages/pagination/index.ts
@@ -7,12 +7,13 @@ import type { Ref } from "vue";
 type TheObject = { [key: string]: any }
 
 export const usePagination = (request: (params: TheObject) => Promise<any>, params?: Ref, config?: any) => {
-  const { listConverter, watchPage = true, afterQuery } = config || {};
+  config = config || {}
+  const { listConverter, watchPage = true } = config || {};
   params = params || ref<TheObject>({});
-  const pageNum = ref(1);
-  const pageSize = ref(20);
+  const num = ref(1);
+  const size = ref(config.size || 20);
   const total = ref(0);
-  const list = ref<[]>([]);
+  const list = ref<any[]>([]);
   const isLoading = ref(false);
 
 
@@ -30,15 +31,12 @@ export const usePagination = (request: (params: TheObject) => Promise<any>, para
     try {
       const queryParams = getParams({
         ...params.value,
-        pageNum: pageNum.value,
-        pageSize: pageSize.value
+        num: num.value,
+        size: size.value
       })
       const { data, total } = getData(await request(queryParams));
       list.value = listSolve(data);
       total.value = total;
-      if (afterQuery) {
-        afterQuery();
-      }
     } catch (error) {
       console.error(error);
     } finally {
@@ -46,45 +44,44 @@ export const usePagination = (request: (params: TheObject) => Promise<any>, para
     }
   };
   const search = () => {
-    pageNum.value = 1;
+    num.value = 1;
     return getPagination();
   };
 
   const refresh = () => getPagination();
   const reset = () => {
-    pageNum.value = 0
+    num.value = 0
     total.value = 0
     list.value = []
   };
 
-  const onPageChange = (num: number) => {
-    pageNum.value = num;
+  const onNumChange = (val: number) => {
+    num.value = val;
     return getPagination();
   };
-  const onSizeChange = (num: number) => {
-    pageSize.value = num;
-    pageNum.value = 1;
+  const onSizeChange = (val: number) => {
+    size.value = val;
+    num.value = 1;
     return getPagination();
   };
 
 
   if (watchPage) {
-    watch(() => pageNum.value, onPageChange);
+    watch(() => num.value, onNumChange);
   }
-
   if (config.immediate) {
     search()
   }
 
   return {
     params,
-    pageNum,
-    pageSize,
+    num,
+    size,
     total,
     list,
     isLoading,
     search,
-    onPageChange,
+    onNumChange,
     onSizeChange,
     refresh,
     reset
@@ -94,6 +91,91 @@ export const usePagination = (request: (params: TheObject) => Promise<any>, para
 
 
 
-export const useContinuous = () => {
+export const useContinuousPagination = (request: (params: TheObject) => Promise<any>, params?: Ref, config?: any) => {
+  config = config || {}
+  const { listConverter, watchPage = true } = config;
+  params = params || ref<TheObject>({});
+  const num = ref(1);
+  const size = ref(config.size || 20);
+  const total = ref(0);
+  const list = ref<any[]>([]);
+  const isLoading = ref(false);
 
-}
+
+  const getParams = config.getParams || ((params: TheObject) => params)
+  const getData = config.getResponse || ((response: TheObject) => response.data)
+
+
+  const listSolve = (arr: any[] | null) => {
+    arr = arr || [];
+    return listConverter ? listConverter(arr) : arr;
+  };
+
+  const getPagination = async () => {
+    isLoading.value = true;
+    try {
+      const queryParams = getParams({
+        ...params.value,
+        num: num.value,
+        size: size.value
+      })
+      const { data, total } = getData(await request(queryParams));
+      if (num.value === 1) {
+        list.value = listSolve(data);
+
+      } else {
+        list.value.push(...listSolve(data));
+      }
+      total.value = total;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+  const search = () => {
+    num.value = 1;
+    return getPagination();
+  };
+
+  const loadNext = (val: number) => {
+    num.value = val;
+    return getPagination();
+  };
+
+  const refresh = async () => {
+    const lastNum = num.value
+    const lastSize = size.value
+    size.value = num.value * lastNum
+    await search
+    num.value = lastNum
+    size.value = lastSize
+
+  };
+  const reset = () => {
+    num.value = 0
+    total.value = 0
+    list.value = []
+  };
+
+  if (watchPage) {
+    watch(() => num.value, loadNext);
+  }
+
+  if (config.immediate) {
+    search()
+  }
+
+  return {
+    params,
+    num,
+    size,
+    total,
+    list,
+    isLoading,
+    search,
+    loadNext,
+    refresh,
+    reset
+  };
+};
